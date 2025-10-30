78dbe52bf1ed2aa17ab3c2b3a44a856a1fd0f2d6
lívia <liviacarvalhofaria6@gmail.com>
2025-10-30 14:14:11 -0300

    30/10/25

--- menu.php
+++ menu.php
@@ -7,6 +7,8 @@
     <div>
     <a href="aluno_cadastro.php">Cadastrar</a>
     <a href="#">Consultar</a>
+    <a href="professor_cadastro.php">Prof. Cadastrar</a>
+    <a href="#">Prof. Consultar</a>
     </div>
 
     </div>
---
+++ professor_cadastro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<?php include "menu.php"?>
+
+<div class="conteudo">
+
+    <h3>Cadastro de Professores</h3>
+
+    <form action="#" method="post">
+
+        <p>
+            Digite o nome <br>
+            <input type="text" name="nome">
+        </p>
+
+        <p>
+            Digite a área de formação <br>
+            <input type="text" name="cidade">
+        </p>
+
+        <p>
+            Digite a graduação <br>
+            <input type="text" name="estado">
+        </p>
+
+        <p>
+            <input type="submit" name="enviar" value="Cadastrar">
+        </p>
+    </form>
+</body>
+</html>
